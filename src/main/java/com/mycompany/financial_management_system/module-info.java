8603541bd3804748c495fec25770d4db3ef05fd8
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-07-28 14:24:21 +0530

    Adding java.sql module for the package

--- src/main/java/com/mycompany/financial_management_system/module-info.java
+++ src/main/java/com/mycompany/financial_management_system/module-info.java
@@ -2,6 +2,7 @@ module com.mycompany.financial_management_system {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.sql;
 
     opens com.mycompany.financial_management_system to javafx.fxml;
     exports com.mycompany.financial_management_system;
