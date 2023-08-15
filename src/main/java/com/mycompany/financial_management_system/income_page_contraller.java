1c34bf7fc5f3fb1bededc0dda8fbfa983205d49f
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-15 12:52:07 +0530

    Getting user income value and save to DB

---
+++ src/main/java/com/mycompany/financial_management_system/income_page_contraller.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.financial_management_system;
+
+
+import com.mycompany.financial_management_system.MySQL_Connection.CheckData;
+import java.io.IOException;
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+/**
+ *FXML Controller class
+ * @author user
+ */
+public class income_page_contraller {
+    
+    
+
+      @FXML
+      private TextField incomeTf;
+            
+    
+    public void add() throws IOException, SQLException{
+    
+        String income2 = incomeTf.getText();
+        double income1 = Double.parseDouble(income2);
+        Buttons addIncome = new Buttons();
+        
+        addIncome.incomeAddButton("wwe",income1);
+         
+    
+        
+      // main_navigation_page_controller addTotalCash = new main_navigation_page_controller();
+       //addTotalCash.changeTotalChash("you3");
+    
+    }
+    
+}
