05b7745b26e038eab1d244bf7b3a6ce42cdc6500
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-15 16:08:02 +0530

    Inserting transport data to transport table in DB

---
+++ src/main/java/com/mycompany/financial_management_system/transport_expenses_page_controller.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.financial_management_system;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author user
+ */
+public class transport_expenses_page_controller {
+    
+     @FXML
+    private TextField from;
+    
+    @FXML
+    private TextField to;
+    
+    @FXML
+    private TextField price;
+    
+    @FXML
+    private TextArea description;
+     
+    public void insertTransportData(String username){
+    
+     LocalDate date = LocalDate.now();
+         java.sql.Date mySqlDate = java.sql.Date.valueOf( date );
+         
+         String t_fromStr = from.getText();
+         String t_toStr = to.getText();
+         String t_priceStr = price.getText();
+         String t_desStr = description.getText();
+         
+         double t_price = Double.parseDouble(t_priceStr);
+      
+     try{
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
+            System.out.println("Connected with the database successfully");
+            PreparedStatement preparedStatement = connection.prepareStatement("insert into transport values(?,?,?,?,?,?)");
+            
+            
+            preparedStatement.setString(1,username);
+            preparedStatement.setDate(2, mySqlDate);
+            preparedStatement.setString(3,t_fromStr );
+            preparedStatement.setString(4,t_toStr);
+            preparedStatement.setDouble(5, t_price);
+            preparedStatement.setString(6, t_desStr);
+           
+            
+            preparedStatement.executeUpdate();
+            System.out.println("Data inserted Successfully");
+                   
+    }catch(SQLException e){
+            System.out.println("Error while connecting to the database");
+    }
+    
+    
+    }
+    
+    
+    
+        public void add(){
+    
+        insertTransportData("MAnula");
+    
+    }
+    
+    
+        public void back() throws IOException{
+            
+              App.setRoot("expenses_navigation_page");
+              
+      
+    }
+
+    
+}
