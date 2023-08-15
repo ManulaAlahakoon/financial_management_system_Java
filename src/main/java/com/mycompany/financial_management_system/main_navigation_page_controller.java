4c200026cb978d9958111e9706e6370e12a1349a
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-15 10:26:46 +0530

    Getting total amount from DB and displaying in totalCash label.

--- src/main/java/com/mycompany/financial_management_system/main_navigation_page_controller.java
+++ src/main/java/com/mycompany/financial_management_system/main_navigation_page_controller.java
@@ -4,13 +4,22 @@
  */
 package com.mycompany.financial_management_system;
 
+import com.mycompany.financial_management_system.MySQL_Connection.CRUD;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -19,10 +28,15 @@ import javafx.stage.Stage;
  */
 public class main_navigation_page_controller {
 
+ 
     /**
      * @throws java.io.IOException
      */
+    @FXML 
+    private Label totalCash;
     
+    
+            
     @FXML
     public void income() throws IOException {
         
@@ -43,7 +57,49 @@ public class main_navigation_page_controller {
     
     
              App.setRoot("expenses_navigation_page");
+             
     }
+    
+   @FXML
+    public String getTotalChash(String username) throws SQLException{
+    
+            //String cash1 = totalCash.getText();
+            //double cash2 = Double.parseDouble(cash1);
+
+           
+            
+         
+          double totalAmount = 0;
+         // double finalTotalAmount = 0;
+          Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
+          
+                      
+           String sqlQuery = "select amount from totalcash  where username = (?)";
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sqlQuery);
+            preparedStatement2.setString(1,username);
+            ResultSet amount =  preparedStatement2.executeQuery();
+           
+                    
+            while(amount.next()){
+            
+                 totalAmount = amount.getDouble("amount");
+               // finalTotalAmount += totalAmount;
+            
+            }
+              String txtValue = Double.toString(totalAmount);
+              
+               return txtValue;
+                 
+       
+      }
+    
+            @FXML
+    public void initialize() throws SQLException{
+            
+        String value = getTotalChash("ttt");
+        totalCash.setText(value);
+    }
+
     
    
     
