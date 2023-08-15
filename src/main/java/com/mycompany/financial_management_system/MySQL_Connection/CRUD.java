bfbc6f405319192f1c0f1ef6cc37e079c7c39e71
ManulaAlahakoon <manulaalahakoon@gmail.com>
2023-08-15 09:51:59 +0530

    Adding a method to update total cash available

--- src/main/java/com/mycompany/financial_management_system/MySQL_Connection/CRUD.java
+++ src/main/java/com/mycompany/financial_management_system/MySQL_Connection/CRUD.java
@@ -4,12 +4,14 @@
  */
 package com.mycompany.financial_management_system.MySQL_Connection;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.*;
 import java.util.Scanner;
+import java.time.LocalDate;
 
 /**
  *
@@ -50,10 +52,72 @@ public class CRUD {
             System.out.println("Error while connecting to the database");
     }
     }
-
-
-}
+     
+      
+     public double totalCashChanging(String username,double income){
+     
+         double finalTotalAmount = 0;
+     
+          try{
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/user","root","root");
+            System.out.println("Connected with the database successfully");
+            PreparedStatement preparedStatement = connection.prepareStatement("update totalcash set amount = (?) where username =(?)");
+            
+           
+           
+           String sqlQuery = "select amount from totalcash  where username = (?)";
+            PreparedStatement preparedStatement2 = connection.prepareStatement(sqlQuery);
+            preparedStatement2.setString(1,username);
+            ResultSet amount =  preparedStatement2.executeQuery();
+            
+            
+            
+                finalTotalAmount += income;
+                    
+            while(amount.next()){
+            
+                double totalAmount = amount.getDouble("amount");
+                finalTotalAmount += totalAmount;
+            
+            }
+                   
+            preparedStatement.setDouble(1,finalTotalAmount);
+            preparedStatement.setString(2, username);
+           
+            preparedStatement.executeUpdate();
+            System.out.println("Data inserted Successfully");
+                   
+    }catch(SQLException e){
+        
+        System.out.println("Error while connecting to the database");
+        
+    }
+     
+     
+     return finalTotalAmount;
+     
+     
+     
+     }
+     
+     }
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+  
     
+        
+    
+    
+ 
+  
     
 
     
